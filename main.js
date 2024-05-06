eec87a822940311148ac491bb3a7d2ab6d5d86d8
luca-de-bar <lucadebar33@gmail.com>
2024-05-06 22:53:31 +0200

    add : start vite server + tailwind

---
+++ main.js
@@ -0,0 +1,8 @@
+﻿import './style.css'
+
+const counterButton = document.getElementById('counter');
+let count = 0;
+counterButton.addEventListener('click', () => {
+  count++;
+  counterButton.textContent = `count is ${count}`;
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+﻿module.exports = {
+  content: [
+    "./index.html",
+    "./main.js",
+    // Aggiungi eventuali altri file HTML o JavaScript che utilizzano classi Tailwind CSS
+  ],
+  theme: {
+    extend: {},
+    // Personalizza il tema se necessario
+  },
+  plugins: [],
+  // Aggiungi eventuali plugin aggiuntivi se necessario
+};
